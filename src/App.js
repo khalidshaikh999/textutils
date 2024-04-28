d102316122773c815896e935e2d11e5a828d4c7d
Khalid <khalidshaikh999@gmail.com>
2024-04-28 15:27:39 +0530

    Added props in component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Navbar />
+    <Navbar title="" aboutText="About us"/>
     </>
   );
 }
