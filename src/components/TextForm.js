90e186e749d5689b18f023d046e60fbff095933d
Khalid <khalidshaikh999@gmail.com>
2024-04-28 17:42:32 +0530

    Added more logic to app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,6 +5,10 @@ const handleUpClick = () => {
     let newText= text.toUpperCase();
     setText(newText)
 }
+const handleLoClick = () => {
+    let newText= text.toLowerCase();
+    setText(newText)
+}
 const handleUpChange = (event) => {
     console.log('on Change')
     setText(event.target.value)
@@ -14,15 +18,27 @@ const handleUpChange = (event) => {
     // setText('Enter your new text here');
 
   return (
-    <div className='container my-3'>
-    <h1 className='text-center mb-3'>{props.heading}</h1>
-        <div className="m-auto col-md-8">
-            <label htmlFor="myBox" className="form-label">{props.labelText}</label>
-            <textarea className="form-control" id="myBox" onChange={handleUpChange} value={text} rows="8"></textarea>
+    <>
+        <div className='container my-3'>
+            <h1 className='text-center mb-3'>{props.heading}</h1>
+            <div className="m-auto col-md-8">
+                <label htmlFor="myBox" className="form-label">{props.labelText}</label>
+                <textarea className="form-control" id="myBox" onChange={handleUpChange} value={text} rows="8"></textarea>
+            </div>
+            <div className="my-3 m-auto col-md-8">
+                <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+            </div>
         </div>
-        <div className="my-3 m-auto col-md-8">
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <div className="container">
+            <div className="col-md-8 m-auto">
+                <h2 className="my-3">Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(' ').length} minutes to read</p>
+                <h2 className="my-3">Your Text Priview</h2>
+                <p>{text}</p>
+            </div>
         </div>
-    </div>
+    </>
   )
 }
