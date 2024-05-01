7fa12f14c9610c9b747f8f41862d0398a00f6dbf
Khalid <khalidshaikh999@gmail.com>
2024-05-01 15:09:39 +0530

    updated repo

--- src/App.js
+++ src/App.js
@@ -1,61 +1,42 @@
 import './App.css';
+import Alerts from './components/Alerts';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React, {useState} from 'react';
 
- 
+
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode==='light'){
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
+      document.body.style.backgroundColor='#042743';
+      showAlert('Dark Mode has been enabled', 'success');
     }
-    else{
+    else {
       setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
+      document.body.style.backgroundColor='#fff';
+      showAlert('Light Mode has been enabled', 'success');
     }
   }
   return (
     <>
-    <Router>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-    <Switch>
-    {/* /users --> Component 1
-        /users/home --> Component 2 */}
-          <Route exact path="/about">
-            <About mode={mode} />
-          </Route>
-          <Route exact path="/">
-            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-          </Route>
-    </Switch>
-    </div>
-    </Router>
-    </> 
+    <Navbar title="TextUtils" aboutText="About us" mode={mode} toggleMode={toggleMode}/>
+    <Alerts alert={alert}/>
+    <TextForm showAlert={showAlert} heading='Enter the text to analyze' labelText='Enter your text below' mode={mode}/>
+    </>
   );
 }
 
